a5eb9680a21f0c7e2a100720a442ff19c958eb68
github maicchan <pororikarori.0321@gmail.com>
2023-06-15 04:51:27 +0900

    LINE BOT作成

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get 'hello/index'
+  post 'callback' => 'line_bot#callback'
+end
